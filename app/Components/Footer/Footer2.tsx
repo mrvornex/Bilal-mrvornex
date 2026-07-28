f15f2010f3506ab660a6bf9ce52190c7dc775983
mrvornex <bilalali.office.pk@gmail.com>
2026-07-29 02:03:17 +0500

    update

--- app/Components/Footer/Footer2.tsx
+++ app/Components/Footer/Footer2.tsx
@@ -15,7 +15,7 @@ const Footer = () => {
           <path d="M22 7.535v9.465a3 3 0 0 1 -2.824 2.995l-.176 .005h-14a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-9.465l9.445 6.297l.116 .066a1 1 0 0 0 .878 0l.116 -.066l9.445 -6.297z"></path>
           <path d="M19 4c1.08 0 2.027 .57 2.555 1.427l-9.555 6.37l-9.555 -6.37a2.999 2.999 0 0 1 2.354 -1.42l.201 -.007h14z"></path>
         </svg>
-        <a href="mailto:ianasnaseeem@gmail.com">bilalali.office.pk@gmail.com</a>
+        <a href="mailto:bilalali.office.pk@gmail.com">bilalali.office.pk@gmail.com</a>
       </div>
     </footer>
   );
